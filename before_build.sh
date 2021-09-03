2bbf46f210b090d57cf069ddb05111ed78e9f4a9
ibook86 <zyh15637540388@gmail.com>
2021-09-04 01:30:33 +0800

    Update before_build.sh

--- before_build.sh
+++ before_build.sh
@@ -3,4 +3,7 @@ cd /home/runner/immortalwrt
 
 # Enter your commands here, e.g.
 # echo "Start build!"
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 make defconfig
