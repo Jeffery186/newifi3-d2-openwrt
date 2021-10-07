726557a0049146ec594743f24602c64653d727a0
ibook86 <zyh15637540388@gmail.com>
2021-10-08 00:39:18 +0800

    add:build-openwrt-immortalwrt-b1.yml

--- before_build.sh
+++ before_build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-cd /home/runner/immortalwrt
 
 # Enter your commands here, e.g.
 # echo "Start build!"
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
-make defconfig
--- clone_source.sh
+++ clone_source.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-git clone "https://github.com/immortalwrt/immortalwrt.git" --branch "openwrt-21.02" --single-branch "immortalwrt"
-pushd "immortalwrt"; ./scripts/feeds update -a; ./scripts/feeds install -a; popd
+git clone -b "openwrt-21.02" --single-branch https://github.com/immortalwrt/immortalwrt
