4c6e650e7bdfbedda72e3ec2c8e9c98d5f91131e
ibook86 <zyh15637540388@gmail.com>
2021-10-08 23:45:17 +0800

    Update:build.sh

--- build.sh
+++ build.sh
@@ -9,5 +9,5 @@ mv ${OP_BUILD_PATH}/.config_immortalwrt ${OP_BUILD_PATH}/immortalwrt/.config
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 make defconfig
 make -j8 download
-make -j$(($(nproc) + 1)) V=s
+make -j$(nproc)
 echo "FILE_DATE=$(date +%Y%m%d%H%M)" >> $GITHUB_ENV
