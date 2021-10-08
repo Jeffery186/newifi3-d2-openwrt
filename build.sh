c91b92983dae61cd3d1e89bca418f4bcf45f869e
Gitea <zyh15637540388@gmail.com>
2021-10-08 15:26:47 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ git clone -b "openwrt-21.02" --single-branch https://github.com/immortalwrt/immo
 
 cd ${OP_BUILD_PATH}/immortalwrt
 ./scripts/feeds update -a && ./scripts/feeds install -a
+rm -rf ./tmp && rm -rf .config
 mv ${OP_BUILD_PATH}/.config_immortalwrt ${BUILD_PATH}/immortalwrt/.config
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
-rm -f .config.old
 make defconfig
 make -j8 download
 make -j$(($(nproc) + 1)) V=s
