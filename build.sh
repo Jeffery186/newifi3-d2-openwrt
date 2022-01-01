fb5984ead92da6b16fb67ab4574b84fdd580c499
ibook86 <zyh15637540388@gmail.com>
2022-01-01 09:24:11 +0800

    增加U盘自动挂载和kmod-tun内核模块

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ git clone -b "openwrt-21.02" --single-branch https://github.com/immortalwrt/immo
 cd ${OP_BUILD_PATH}/immortalwrt
 ./scripts/feeds update -a && ./scripts/feeds install -a
 rm -rf ./tmp && rm -rf .config
-mv ${OP_BUILD_PATH}/.config_immortalwrt ${OP_BUILD_PATH}/immortalwrt/.config
+mv ${OP_BUILD_PATH}/ssr.config ${OP_BUILD_PATH}/immortalwrt/.config
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 make defconfig
-make -j8 download
+make -j64 download
 make -j$(nproc)
-echo "FILE_DATE=$(date +%Y%m%d%H%M)" >> $GITHUB_ENV
+echo "FILE_DATE=$(date +%Y%m%d%H%M)" >>$GITHUB_ENV
